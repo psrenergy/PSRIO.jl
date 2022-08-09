b7c1b470efa0b107699cb5ae21a0cc6f0b91635a
guilhermebodin <guilherme.b.moraes@gmail.com>
2022-08-09 16:58:14 -0300

    add ci

--- scripts/update_artifacts.bat
+++ scripts/update_artifacts.bat
@@ -1,20 +1,39 @@
-del /q ".\Project.toml"
-del /q ".\Manifest.toml"
-rmdir /S /Q ".\psrio-distribution"
-rmdir /S /Q ".\ArtifactsGenerator.jl"
+@echo off
 
-git clone -b develop "https://bitbucket.org/psr/psrio-distribution.git"
-git clone "https://github.com/psrenergy/ArtifactsGenerator.jl.git"
+set basepath=%~dp0
+set AWS_KEY=%1
+set SECRET_AWS_KEY=%2
 
-copy ".\ArtifactsGenerator.jl\Project.toml" "Project.toml"
+echo "Cloning psrio-distribution"
+git clone --depth=1 -b develop "https://bitbucket.org/psr/psrio-distribution.git" "%basepath%psrio-distribution"
 
-julia --project ./update_artifacts.jl  "psrio-distribution" "PSRIO" "..\Artifacts.toml" "..\Project.toml"
+cd %basepath%psrio-distribution
 
-del /q ".\Project.toml"
-del /q ".\Manifest.toml"
-rmdir /S /Q ".\psrio-distribution"
-rmdir /S /Q ".\ArtifactsGenerator.jl"
+git log --pretty=format:%%h > %temp%\git.txt
+for /f "Tokens=*" %%a in (%temp%\git.txt) do @set last_commit=%%a
 
-cd .. 
-julia --project -e "import Pkg; Pkg.test()"
-cd scripts
+copy %basepath%psrio-distribution\linux\psrio.ver %basepath%\psrio.ver
+
+echo "Cloning ArtifactsGenerator"
+git clone --depth=1 -b 0.2.0 "https://github.com/psrenergy/ArtifactsGenerator.jl.git" "%basepath%ArtifactsGenerator.jl"
+
+copy "%basepath%\ArtifactsGenerator.jl\Project.toml" "%basepath%Project.toml"
+copy "%basepath%\awsintegration\lib\lin64\libawsinterface.so" "%basepath%\psrclassesinterfacejulia\\"
+
+echo "Updating artifatcs"
+call %JULIA_161% --color=yes --project %basepath%update_artifacts.jl "%basepath%psrio-distribution" "psrio-distribution" "PSRIO" "%basepath%../Artifacts.toml" "%AWS_KEY%" "%SECRET_AWS_KEY%" "%last_commit%"
+IF %ERRORLEVEL% NEQ 0 (
+    EXIT 1
+)
+
+echo "Cleaning directory"
+rmdir /S /Q "%basepath%\psrio-distribution"
+rmdir /S /Q "%basepath%\ArtifactsGenerator.jl"
+del /q "%basepath%\Project.toml"
+del /q "%basepath%\Manifest.toml"
+
+echo "Testing package"
+%JULIA_161% --color=yes --project="%basepath%.." -e "import Pkg; Pkg.test()"
+IF %ERRORLEVEL% NEQ 0 (
+    EXIT 1
+)
