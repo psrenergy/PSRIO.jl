19051e697a8eb25390e5c308e66a8101b3fef191
guilhermebodin <guilherme.b.moraes@gmail.com>
2022-12-07 11:18:50 -0300

    Update artifacts generator script

--- scripts/update_artifacts.bat
+++ scripts/update_artifacts.bat
@@ -9,18 +9,15 @@ git clone --depth=1 -b develop "https://bitbucket.org/psr/psrio-distribution.git
 
 cd %basepath%psrio-distribution
 
-git log --pretty=format:%%h > %temp%\git.txt
-for /f "Tokens=*" %%a in (%temp%\git.txt) do @set last_commit=%%a
-
 copy %basepath%psrio-distribution\linux\psrio.ver %basepath%\psrio.ver
 
 echo "Cloning ArtifactsGenerator"
-git clone --depth=1 -b 0.2.0 "https://github.com/psrenergy/ArtifactsGenerator.jl.git" "%basepath%ArtifactsGenerator.jl"
+git clone --depth=1 -b 0.3.0 "https://github.com/psrenergy/ArtifactsGenerator.jl.git" "%basepath%ArtifactsGenerator.jl"
 
 copy "%basepath%\ArtifactsGenerator.jl\Project.toml" "%basepath%Project.toml"
 
-echo "Updating artifacts"
-call %JULIA_161% --color=yes --project %basepath%update_artifacts.jl "%basepath%psrio-distribution" "psrio-distribution" "PSRIO" "%basepath%../Artifacts.toml" "%AWS_KEY%" "%SECRET_AWS_KEY%" "%last_commit%"
+echo "Updating artifatcs"
+call %JULIA_161% --color=yes --project %basepath%update_artifacts.jl "%basepath%psrio-distribution" "psrio-distribution" "PSRIO" "%basepath%../Artifacts.toml" "%AWS_KEY%" "%SECRET_AWS_KEY%"
 IF %ERRORLEVEL% NEQ 0 (
     EXIT 1
 )
