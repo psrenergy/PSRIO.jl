b43d1c83ba2e3349299e935a351a6b9e2a39bce3
guilhermebodin <guilherme.b.moraes@gmail.com>
2024-01-10 11:22:07 -0300

    Non lazy artifacts

--- scripts/update_artifacts.bat
+++ scripts/update_artifacts.bat
@@ -19,12 +19,10 @@ CD %BASE_PATH%psrio-distribution
 COPY %BASE_PATH%psrio-distribution\linux\PSRIO.ver %BASE_PATH%\PSRIO.ver
 
 ECHO "Cloning ArtifactsGenerator"
-git clone --depth=1 -b 0.3.0 "https://github.com/psrenergy/ArtifactsGenerator.jl.git" "%BASE_PATH%ArtifactsGenerator.jl"
-
-COPY "%BASE_PATH%\ArtifactsGenerator.jl\Project.toml" "%BASE_PATH%Project.toml"
+git clone --depth=1 -b 0.5.0 "https://github.com/psrenergy/ArtifactsGenerator.jl.git" "%BASE_PATH%ArtifactsGenerator.jl"
 
 ECHO "Updating artifatcs"
-CALL %JULIA_161% --color=yes --project %BASE_PATH%update_artifacts.jl "%BASE_PATH%psrio-distribution" "psrio-distribution" "PSRIO" "%BASE_PATH%../Artifacts.toml" "%AWS_KEY%" "%SECRET_AWS_KEY%"
+CALL %JULIA_161% --color=yes --project=%BASE_PATH%ArtifactsGenerator.jl %BASE_PATH%update_artifacts.jl "%BASE_PATH%psrio-distribution" "psrio-distribution" "PSRIO" "%BASE_PATH%../Artifacts.toml" "%AWS_KEY%" "%SECRET_AWS_KEY%"
 IF %ERRORLEVEL% NEQ 0 (
     EXIT 1
 )
@@ -32,8 +30,6 @@ IF %ERRORLEVEL% NEQ 0 (
 ECHO "Cleaning directory"
 RMDIR /S /Q "%BASE_PATH%\psrio-distribution"
 RMDIR /S /Q "%BASE_PATH%\ArtifactsGenerator.jl"
-DEL /q "%BASE_PATH%\Project.toml"
-DEL /q "%BASE_PATH%\Manifest.toml"
 
 ECHO "Testing package"
 %JULIA_161% --color=yes --project="%BASE_PATH%.." -e "import Pkg; Pkg.test()"
