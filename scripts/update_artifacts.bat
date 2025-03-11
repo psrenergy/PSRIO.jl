d4a313f39102212f6cebd0e811e10250cd1d3bec
raphasampaio <raphaelaraujosampaio@gmail.com>
2025-03-11 18:40:13 -0300

    Updated

--- scripts/update_artifacts.bat
+++ scripts/update_artifacts.bat
@@ -12,7 +12,7 @@ DEL /q "%BASE_PATH%\Project.toml"
 DEL /q "%BASE_PATH%\Manifest.toml"
 
 ECHO "Cloning psrio-distribution"
-git clone --depth=1 -b develop "https://bitbucket.org/psr/psrio-distribution.git" "%BASE_PATH%psrio-distribution"
+git clone --depth=1 -b test_load_save_genesys "https://bitbucket.org/psr/psrio-distribution.git" "%BASE_PATH%psrio-distribution"
 
 CD %BASE_PATH%psrio-distribution
 
@@ -22,7 +22,7 @@ ECHO "Cloning ArtifactsGenerator"
 git clone --depth=1 -b 0.5.0 "https://github.com/psrenergy/ArtifactsGenerator.jl.git" "%BASE_PATH%ArtifactsGenerator.jl"
 
 ECHO "Updating artifatcs"
-CALL %JULIA_161% --color=yes --project=%BASE_PATH%ArtifactsGenerator.jl %BASE_PATH%update_artifacts.jl "%BASE_PATH%psrio-distribution" "psrio-distribution" "PSRIO" "%BASE_PATH%../Artifacts.toml" "%AWS_KEY%" "%SECRET_AWS_KEY%"
+CALL %JULIA_166% --color=yes --project=%BASE_PATH%ArtifactsGenerator.jl %BASE_PATH%update_artifacts.jl "%BASE_PATH%psrio-distribution" "psrio-distribution" "PSRIO" "%BASE_PATH%../Artifacts.toml" "%AWS_KEY%" "%SECRET_AWS_KEY%"
 IF %ERRORLEVEL% NEQ 0 (
     EXIT 1
 )
@@ -32,7 +32,7 @@ RMDIR /S /Q "%BASE_PATH%\psrio-distribution"
 RMDIR /S /Q "%BASE_PATH%\ArtifactsGenerator.jl"
 
 ECHO "Testing package"
-%JULIA_161% --color=yes --project="%BASE_PATH%.." -e "import Pkg; Pkg.test()"
+%JULIA_166% --color=yes --project="%BASE_PATH%.." -e "import Pkg; Pkg.test()"
 IF %ERRORLEVEL% NEQ 0 (
     EXIT 1
 )
