dd4b1593007c2f27177b76ee31a35f5ab0638856
Iury <iurysaboia927@gmail.com>
2026-03-06 08:43:24 -0300

    Add develop push option

---
+++ scripts/commit_new_updates_to_develop.bat
@@ -0,0 +1,10 @@
+@echo off
+
+set basepath=%~dp0
+
+for /f "Tokens=*" %%a in (%basepath%\PSRIO.ver) do @set psrio_version=%%a
+git config --local user.name "GithubPSRIOUpdater"
+git add %basepath%..\Artifacts.toml 
+git add %basepath%..\Project.toml 
+git commit -m "[PSRIO update] version %psrio_version%"
+git push origin develop
